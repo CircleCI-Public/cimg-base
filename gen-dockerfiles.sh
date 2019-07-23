b6fd9d71c5a16dcc9574d933db2ac7f9f6bad65a
iynere <iynere@users.noreply.github.com>
2019-07-23 12:32:01 -0400

    remove unused files

--- gen-dockerfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-version=( "$@" )
-declare -a variants=("node")
-
-repo="cimg/base:"
-
-sed -r -e 's!%%BASE_VERSION%%!'"$version"'!g' "ubuntu/Dockerfile.template" > "ubuntu/Dockerfile"
