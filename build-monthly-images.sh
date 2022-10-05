a9406f4c93b76732a92b6ee7a340e2e0014366f0
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-04 20:50:35 -0400

    Prep new monthly release (#212)
    This includes the work needed to publish the monthly updates but with
    support for arm64 images. This work is partially manual and doesn't fit
    in with the fully automated system we normally use for cimg-shared. This
    is okay for now because:
    
    1. it's temporary as work continues in cimg-shared for better arm
    support
    2. This image was always slightly an oddball anyway since it's the base
    image.
    
    I'm hoping this have this system more automated by the November release.

---
+++ build-monthly-images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker context create cimg
+docker buildx create --use cimg
+
+VERSION=$( date +%Y.%m )
+
+echo "The 'version' is ${VERSION}"
+
+docker buildx build --platform=linux/amd64,linux/arm64 --file 18.04/Dockerfile -t cimg/base:${VERSION}-18.04 -t cimg/base:current-18.04 .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 20.04/Dockerfile -t cimg/base:${VERSION}-20.04 -t cimg/base:current-20.04 .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 22.04/Dockerfile -t cimg/base:${VERSION}-22.04 -t cimg/base:${VERSION} -t cimg/base:current-22.04 -t cimg/base:current .
