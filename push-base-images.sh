396d29be738f4d4de632036599b328011d8dd063
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-03-06 11:05:02 -0500

    Drop support for Ubuntu 18.04

--- push-base-images.sh
+++ push-base-images.sh
@@ -3,6 +3,5 @@
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --push --platform=linux/amd64,linux/arm64 --file 18.04/Dockerfile -t cimg/base:edge-18.04
 docker buildx build --push --platform=linux/amd64,linux/arm64 --file 20.04/Dockerfile -t cimg/base:edge-20.04
 docker buildx build --push --platform=linux/amd64,linux/arm64 --file 22.04/Dockerfile -t cimg/base:edge-22.04 -t cimg/base:edge .
--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/base:18.04
 docker push cimg/base:20.04
 docker push cimg/base:22.04
