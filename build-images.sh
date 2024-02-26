4540374474fd8db31578a7530d5dd517ad3877fd
Josh Bryans <josh.bryans@circleci.com>
2024-02-26 09:15:46 -0500

    Update depdency versions

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
 docker context create cimg
 docker buildx create --use cimg
 docker buildx build --platform=linux/amd64,linux/arm64 --file 20.04/Dockerfile -t cimg/base:20.04 -t cimg/base:20.04 .
