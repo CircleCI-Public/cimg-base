6c0cbfda09a3292a5754d59f3179cb6d7767b268
Josh Bryans <josh.bryans@circleci.com>
2024-02-29 11:41:51 -0500

    Build with buildx

--- build-images.sh
+++ build-images.sh
@@ -3,5 +3,5 @@
 
 docker context create cimg
 docker buildx create --use cimg
-docker build --file 20.04/Dockerfile -t cimg/base:20.04 -t cimg/base:20.04 --platform=linux/amd64,linux/arm64 .
-docker build --file 22.04/Dockerfile -t cimg/base:22.04 -t cimg/base:22.04 --platform=linux/amd64,linux/arm64 .
+docker buildx build --file 20.04/Dockerfile -t cimg/base:20.04 -t cimg/base:20.04 --platform=linux/amd64,linux/arm64 .
+docker buildx build --file 22.04/Dockerfile -t cimg/base:22.04 -t cimg/base:22.04 --platform=linux/amd64,linux/arm64 .
