fa26184ad70e8ce9e67404b566ecefd466e913f0
marboledacci <mateo.arboleda@circleci.com>
2025-05-20 12:43:40 -0500

    Add version 24.04 and updated workflow to use cimg orb (#270)
    For our official CircleCI Docker Convenience Image support policy,
    please see [CircleCI
    docs](https://circleci.com/docs/convenience-images-support-policy).
    
    This policy outlines the release, update, and deprecation policy for
    CircleCI Docker Convenience Images.
    
    ---
    
    # Description
    - Add version 24.04
    - Update shared module
    - Update workflow to use cimg
    
    # Reasons
    The 24.04 is being requested and to add it in the better way, and keep
    this repo updated the shared repo was moved to the newer version and the
    required changes for it to work were made.
    # Checklist
    
    Please check through the following before opening your PR. Thank you!
    
    - [ ] I have made changes to the `Dockerfile.template` file only
    - [ ] I have not made any manual changes to automatically generated
    files
    - [ ] My PR follows best practices as described in the [contributing
    guidelines](CONTRIBUTING)
    - [ ] (Optional, but recommended) My commits are
    [signed](https://docs.github.com/en/authentication/managing-commit-signature-verification/signing-commits)

--- push-base-images.sh
+++ push-base-images.sh
@@ -5,3 +5,4 @@ docker context create cimg
 docker buildx create --use cimg
 docker buildx build --push --platform=linux/amd64,linux/arm64 --file 20.04/Dockerfile -t cimg/base:edge-20.04
 docker buildx build --push --platform=linux/amd64,linux/arm64 --file 22.04/Dockerfile -t cimg/base:edge-22.04 -t cimg/base:edge .
+docker buildx build --push --platform=linux/amd64,linux/arm64 --file 24.04/Dockerfile -t cimg/base:edge-24.04
--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/base:20.04
-docker push cimg/base:22.04
+set -eo pipefail
