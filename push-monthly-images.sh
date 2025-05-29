bbb463313899109b6e3fc1dc28b34acdd7beaf72
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-05-29 11:16:08 -0500

    Update default version to 24

--- push-monthly-images.sh
+++ push-monthly-images.sh
@@ -6,5 +6,5 @@ docker buildx create --use cimg
 
 VERSION=$( date +%Y.%m )
 
-docker buildx build --push --platform=linux/amd64,linux/arm64 --file 22.04/Dockerfile -t cimg/base:${VERSION}-22.04 -t cimg/base:${VERSION} -t cimg/base:current-22.04 -t cimg/base:current .
-docker buildx build --push --platform=linux/amd64,linux/arm64 --file 24.04/Dockerfile -t cimg/base:${VERSION}-24.04 -t cimg/base:current-24.04 .
+docker buildx build --push --platform=linux/amd64,linux/arm64 --file 22.04/Dockerfile -t cimg/base:${VERSION}-22.04 -t cimg/base:current-22.04 .
+docker buildx build --push --platform=linux/amd64,linux/arm64 --file 24.04/Dockerfile -t cimg/base:${VERSION}-24.04 -t cimg/base:${VERSION} -t cimg/base:current-24.04 -t cimg/base:current .
