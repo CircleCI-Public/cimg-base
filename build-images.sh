4fc05e16ae8ed9be5de71064f682db0f7bff13bd
Anthony Castro <anthony.castro@circleci.com>
2026-07-29 17:12:42 -0400

    Add ubuntu 26.04

--- build-images.sh
+++ build-images.sh
@@ -6,3 +6,4 @@ docker context create cimg
 docker buildx create --use cimg
 docker buildx build --platform=linux/amd64,linux/arm64 --file 22.04/Dockerfile -t cimg/base:22.04 -t cimg/base:22.04 --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file 24.04/Dockerfile -t cimg/base:24.04 -t cimg/base:24.04 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 26.04/Dockerfile -t cimg/base:26.04 -t cimg/base:26.04 --push .
